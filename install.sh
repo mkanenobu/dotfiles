31754ab3c4fdf7d7d1a83f4ee521f5d4cc99c767
mkanenobu <kaneda.1018@gmail.com>
2016-12-08 23:02:54 +0900

    fix

--- install.sh
+++ install.sh
@@ -2,6 +2,11 @@
 #!/bin/bash
 if [ ! -e $HOME/git/ ]; then
 	mkdir -p $HOME/git/
+	mkdir -p $HOME/git/dotfiles/
 fi
 ls -a $HOME/git/dotfiles/.??* --ignore=".git" | xargs cp -t $HOME/ -rfv
+
+if [ ! -e $HOME/.config/nvim/ ]; then
+	mkdir -p $HOME/.config/nvim/
+fi
 cd -fv $HOME/git/dotfiles/init.vim $HOME/.config/nvim/
