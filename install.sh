63cfc86a858a0b31762966a90a28797586b5d097
mkanenobu <kaneda.1018@gmail.com>
2017-02-10 00:07:32 +0900

    install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ### install dotfiles
-DOTDIR=$HOME/Git/dotfiles
+DOTDIR="$HOME"/Git/dotfiles
 ln -sv "$DOTDIR"/.bashrc ~/.bashrc
 ln -sv "$DOTDIR"/.inputrc ~/.inputrc
 ln -sv "$DOTDIR"/.profile ~/.profile
