249ec69b1775f64107d20d6bf8373227ae1761a3
mkanenobu <kaneda.1018@gmail.com>
2017-04-29 23:42:36 +0900

    some shanges

--- install.sh
+++ install.sh
@@ -13,8 +13,8 @@ fi
 ln -v "$DOTDIR"/.profile ~/.profile
 #ln -sv "$DOTDIR"/user_dictionary.db ~/.mozc/user_dictionary.db
 ln -sv "$DOTDIR"/.dein ~/.dein
-if [! -e ~/.config/nvim ];then
-	mkdir -p ~/.config
+if [ ! -e ~/.config/nvim ];then
+	mkdir -p ~/.config/nvim
 fi
 ln -sv "$DOTDIR"/init.vim ~/.config/nvim/init.vim
 ln -sv "$DOTDIR"/byobu ~/.config/byobu
